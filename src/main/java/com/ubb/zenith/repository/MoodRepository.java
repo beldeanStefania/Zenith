e89783b836cd157ee627a41edfe1a47f93670774
Alexandra-badescu1 <118006049+Alexandra-badescu1@users.noreply.github.com>
2024-10-19 19:23:57 +0300

    ZNTH 45 si 47

--- src/main/java/com/ubb/zenith/repository/MoodRepository.java
+++ src/main/java/com/ubb/zenith/repository/MoodRepository.java
@@ -3,5 +3,8 @@ package com.ubb.zenith.repository;
 import com.ubb.zenith.model.Mood;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MoodRepository extends JpaRepository<Mood, Integer> {
+   // Object findByEnergy_scoreAndHappiness_scoreAndLove_scoreAndSadness_score(Integer energyScore, Integer happinessScore, Integer loveScore, Integer sadnessScore);
 }
