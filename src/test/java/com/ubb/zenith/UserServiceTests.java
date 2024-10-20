9eff654ce4343df4e80a13a9f9efd49eaf61bb11
aluas21 <alinaluasdoru@gmail.com>
2024-10-20 19:37:24 +0300

    ZNTH 38/42/46

--- src/test/java/com/ubb/zenith/UserServiceTests.java
+++ src/test/java/com/ubb/zenith/UserServiceTests.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-class UserServiceTests {
+class girUserServiceTests {
 
     @Mock
     private UserRepository userRepository;
